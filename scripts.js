c9d05968f9f31133c58d1c8f82e0d675943208da
Guilherme <gui_7531@hotmail.com>
2022-03-31 13:42:33 -0300

    siteContabil

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let formulario = document.getElementById("formulario-contato")
+let linkContato = document.getElementById("link-contato")
+
+function mostrarFormulario() {
+    formulario.className = "mostrar-formulario"
+}
+
+
+
+
+
+linkContato.addEventListener("click", mostrarFormulario)
